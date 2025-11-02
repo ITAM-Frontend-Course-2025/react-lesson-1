f8ac271b44445657d1713166d4f1783005976517
Mikhail Volodko <clavor.32@gmail.com>
2025-11-03 02:29:36 +0300

    add useEffect for saving in localStorage

--- itam-react-hm1/src/App.tsx
+++ itam-react-hm1/src/App.tsx
@@ -1,10 +1,22 @@
 import Header from "./layouts/Header/Header.tsx";
 import Main from "./layouts/Main/Main.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const App = () => {
-  const [textName, setTextName] = useState('Миша')
+  const [textName, setTextName] = useState(() => {
+    const savedName = localStorage.getItem('name')
+
+    if(savedName) {
+      return JSON.parse(savedName)
+    }
+
+    return 'Миша'
+  })
   const [isEditing, setIsEditing] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('name',JSON.stringify(textName))
+  }, [textName]);
 
   return (
     <>
